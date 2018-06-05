83f9d01062c1183005905be64291e3112edda58f
Vincent Moeykens <vpam99@gmail.com>
2018-06-05 15:57:06 -0400

    Adds quotes to Aaron B

--- app.py
+++ app.py
@@ -5,21 +5,21 @@ import random
 app = Flask(__name__)
 api = Api(app)
 
-quotes = ["You also have permission to tickle Tom now",
-          "Well fudge",
-          "Damn it, that's what I get for working in situ on VIM.  VPN dropped",
-          "Yeah, turns out, I'm just a horse's a**",
-          "Who...am I?",
-          "Ha, Oh, I'm always watching",
-          "I bet if we summoned Linus Torvalds, first, we notice what a f*cking weird dude he is",
-          "I will slap someone if you don't get a UAT response by end of day",
-          "Haha, damn it. GET OUT OF MY HEAD!!!",
-          "I have a spreadsheet titled 'PB Bureaucracy Navigation'",
-          "Well, Gabby just got up, walked across the living room (the big event)....and slapped the dog",
-          "Just saw a dude with a giant juggalo tattoo biking next to his friend with a duster and matrix glasses going by...my day might need to end",
-          "If I type 'cocker'....one more time",
-          "As a capitalist merc with proletariat tendencies, I endorse this private room",
-          "Don't start projects you don't plan on finishing (Or Something Like That)",]
+quotes = ["'You also have permission to tickle Tom now' - Aaron B",
+          "'Well fudge' - Aaron B",
+          "'Damn it, that's what I get for working in situ on VIM.  VPN dropped' - Aaron B",
+          "'Yeah, turns out, I'm just a horse's a**' - Aaron B",
+          "'Who...am I?' - Aaron B",
+          "'Ha, Oh, I'm always watching' - Aaron B",
+          "'I bet if we summoned Linus Torvalds, first, we notice what a f*cking weird dude he is' - Aaron B",
+          "'I will slap someone if you don't get a UAT response by end of day' - Aaron B",
+          "'Haha, damn it. GET OUT OF MY HEAD!!!' - Aaron B",
+          "'I have a spreadsheet titled 'PB Bureaucracy Navigation'' - Aaron B",
+          "W'ell, Gabby just got up, walked across the living room (the big event)....and slapped the dog' - Aaron B",
+          "'Just saw a dude with a giant juggalo tattoo biking next to his friend with a duster and matrix glasses going by...my day might need to end' - Aaron B",
+          "'If I type 'cocker'....one more time' - Aaron B",
+          "'As a capitalist merc with proletariat tendencies, I endorse this private room' - Aaron B",
+          "'Don't start projects you don't plan on finishing (Or Something Like That)' - Aaron B",]
 
 
 class User(Resource):
